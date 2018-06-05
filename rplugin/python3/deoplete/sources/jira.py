ab34324cb9c45ba5dc5831842bee3462815d7891
Yuri Bochkarev <baltazar.bz@gmail.com>
2018-06-05 10:52:39 +0300

    use jira completer based on matcherkey deoplete functionality

--- rplugin/python3/deoplete/sources/jira.py
+++ rplugin/python3/deoplete/sources/jira.py
@@ -13,25 +13,25 @@ def log(msg):
     with open('/tmp/deoplete-jira.log', 'a') as file_:
         file_.write('%s\n' % msg)
 
-from jira_rt_completion_server.jira_completer import JiraCompleter
+from jira_rt_completion_server.jira_completer import JiraCompleter, JiraCompleterMatcherKey
 
 
 class Source(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
 
-        self._completer = JiraCompleter('~/.cache/jira/jira.candidates.tsv')
+        self._completer = JiraCompleterMatcherKey('~/.cache/jira/jira.candidates.tsv')
 
-        self.debug_enabled = True
+        #self.debug_enabled = True
         self.name = 'jira'
         #self.kind = 'keyword'
         self.mark = '[JIRA]'
         #self.min_pattern_length = 2
 
         # Use these options if you want to filter candidates yourself
-        self.is_volatile = True
-        self.matchers = [] # ['matcher_cpsm']
-        self.sorters = []
+        #self.is_volatile = True
+        #self.matchers = [] # ['matcher_cpsm']
+        #self.sorters = []
 
         # Use these options if you want to implement custom matcher
         #self.matchers = ['matcher_fuzzy', 'matcher_full_fuzzy']
@@ -41,6 +41,7 @@ class Source(Base):
         self.max_menu_width = 150
         self.max_abbr_width = 150
         self.input_pattern = self._completer.input_pattern #r'JI:?\w*$' #self._source.input_pattern
+        self.matcher_key = 'custom_key'
 
     def get_complete_position(self, context):
         return self._completer.get_complete_position(context)
@@ -48,5 +49,38 @@ class Source(Base):
     def gather_candidates(self, context):
         return self._completer.gather_candidates(context)
 
-    def on_post_filter(self, context):
-        return self._completer.on_post_filter(context)
+
+# class Source(Base):
+#     def __init__(self, vim):
+#         Base.__init__(self, vim)
+#
+#         self._completer = JiraCompleter('~/.cache/jira/jira.candidates.tsv')
+#
+#         self.debug_enabled = True
+#         self.name = 'jira'
+#         #self.kind = 'keyword'
+#         self.mark = '[JIRA]'
+#         #self.min_pattern_length = 2
+#
+#         # Use these options if you want to filter candidates yourself
+#         self.is_volatile = True
+#         self.matchers = [] # ['matcher_cpsm']
+#         self.sorters = []
+#
+#         # Use these options if you want to implement custom matcher
+#         #self.matchers = ['matcher_fuzzy', 'matcher_full_fuzzy']
+#         #self.sorters = ['sorter_rank']
+#         #self.converters = []
+#
+#         self.max_menu_width = 150
+#         self.max_abbr_width = 150
+#         self.input_pattern = self._completer.input_pattern #r'JI:?\w*$' #self._source.input_pattern
+#
+#     def get_complete_position(self, context):
+#         return self._completer.get_complete_position(context)
+#
+#     def gather_candidates(self, context):
+#         return self._completer.gather_candidates(context)
+#
+#     def on_post_filter(self, context):
+#         return self._completer.on_post_filter(context)
